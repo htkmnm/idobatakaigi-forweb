409237057a2b5399fff45b6d849be32229fb1ec4
はつき みなみ <hatsukiminami@hatsuki-no-MacBook-Air.local>
2020-11-17 19:43:31 +0900

    default

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+import Login from './pages/Login';
+import Main from './pages/Main';
+
+function App() {
+  const [name, setName] = useState('')
+
+  if (name.length === 0) {
+    return <Login setName={setName} />
+  } else {
+    return <Main name={name} />
+  };
+};
+
+
+
+export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const Login = ({ setName }) => {
+    const [string, setString] = useState('');
+
+    const handleClick = () => {
+        setName(string)
+    };
+
+    return (
+        <div className='login'>
+            <h1>井戸端会議 for web</h1>
+            <TextField id="name" label="お名前" value={string} onChange={e => setString(e.target.value)} /><br />
+            <Button variant="outlined" onClick={handleClick}>ログイン</Button>
+        </div>
+    );
+};
+
+export default Login;
