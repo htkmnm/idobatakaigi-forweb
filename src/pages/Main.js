fddb810e7192d36e40486471a4f32408a1f68967
はつき みなみ <hatsukiminami@hatsuki-no-MacBook-Air.local>
2020-12-27 20:07:24 +0900

    chatapp

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,32 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { generateGravatar } from '../gravatar';
+import Avatar from '@material-ui/core/Avatar';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { sendMessage, readData } from '../config/firebase';
+import { sendMessage, db } from '../config/firebase';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Main.css';
 
 const Main = ({ name }) => {
     const [string, setString] = useState('');
-    const [data, setData] = useState([]);
+    const [avatarurl, setAvatarurl] = useState('');
+    const [message, setMessage] = useState();
 
     const handleClick = async () => {
         sendMessage(name, string)
-        await readData()
-        setData(data);
-        console.log(data)
+        readData()
     };
+
+    useEffect(() => {
+        const url = generateGravatar(name)
+        setAvatarurl(url);
+        readData()
+    }, []);
+
+    const readData = async () => {
+        const tempArray = []
+        await db
+            .collection('messages')
+            .orderBy('createAt', 'asc')
+            .get()
+            .then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    console.log(doc.id, " => ", doc.data());
+                    tempArray.push(doc.data())
+                });
+                setMessage(tempArray)
+            })
+    };
+    console.log(message)
 
     return (
         <div>
-            {data.map((element, index) => {
-                return (
-                    <ul key={index}>
-                        <li>{element.name}</li>
-                        <li>{element.message}</li>
-                        <li>{element.createAt}</li>
-                    </ul>
-                );
-            })}
-            <footer>
-                <TextField id="message" label="message" value={string} onChange={e => setString(e.target.value)} />
+            <header>aaa</header>
+            <main className='ue'>
+                {message && message.map((element, index) => {
+                    return (
+                        <ul key={index}>
+                            <li className='kesu'>
+                                <Avatar alt={name} src={generateGravatar(element.name)} />
+                                <div className='namesize'>{element.name}</div><br />{element.message}
+                            </li>
+                        </ul>
+                    );
+                })}
+            </main>
+            <footer className='yoko'>
+                <div>
+                    <Avatar alt={name} src={generateGravatar(name)} />
+                    <div className='namesize'>{name}</div>
+                </div>
+                <TextField className='text' id="text" label="text" value={string} onChange={e => setString(e.target.value)} />
                 <Button variant="outlined" onClick={handleClick}>送信</Button>
             </footer>
         </div>
